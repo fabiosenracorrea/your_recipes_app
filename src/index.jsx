cff3d40d0974aa3aee7184f009f11f3df41ebe37
Fábio Senra Corrêa <fabiosenracorrea@hotmail.com>
2020-12-13 21:54:59 -0300

    paging CSS abstracted to single file

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,7 @@ import * as serviceWorker from './serviceWorker';
 import App from './App';
 
 import './styles/global.css';
+import './styles/paging.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
