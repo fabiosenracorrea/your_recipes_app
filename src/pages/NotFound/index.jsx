64585fde00d3e828f8739380d898348bfa8bbd75
Fábio Senra Corrêa <fabiosenracorrea@hotmail.com>
2020-12-15 20:39:45 -0300

    removing unecessary check

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
-import { useLocation, Redirect, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { FaSadTear } from 'react-icons/fa';
 
 import './styles.css';
 
 function NotFound() {
-  const { pathname } = useLocation();
-
-  if (pathname === '/Cocktails') {
-    return (
-      <Redirect to="/cocktails" />
-    );
-  }
-
   return (
     <div className="not-found-page">
       <div className="not-found-header">
