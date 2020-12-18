ec072fcc7c8d4c62a88825ed766a336a1c23e657
Fábio Senra Corrêa <fabiosenracorrea@hotmail.com>
2020-12-18 19:41:43 -0300

    done/favorite recipes styling adjustment for 0 recipes

--- src/pages/DoneRecipes/index.jsx
+++ src/pages/DoneRecipes/index.jsx
@@ -23,9 +23,9 @@ function DoneRecipes() {
 
   const filteredItems = useMemo(() => {
     switch (filter) {
-    case 'Foods':
+    case 'meals':
       return doneRecipes.filter((recipe) => recipe.type === 'meals');
-    case 'Drinks':
+    case 'cocktails':
       return doneRecipes.filter((recipe) => recipe.type === 'cocktails');
     default:
       return doneRecipes;
@@ -38,6 +38,8 @@ function DoneRecipes() {
     setFilter(filterClicked);
   }, []);
 
+  const selectedFilterIsCocktails = useMemo(() => filter === 'cocktails', [filter]);
+
   return (
     <div className="done-recipes-page">
       <Header pageTitle="Done Recipes" />
@@ -47,75 +49,87 @@ function DoneRecipes() {
         handleFilterChange={ handleFilterChange }
       />
 
-      <div className="done-recipes-container">
-        {filteredItems.map((recipe, index) => (
-          <div className="done-recipe-card" key={ recipe.doneDate.toLocaleString() }>
-            <Link to={ `/${recipe.type}/${recipe.id}` }>
-              <img
-                src={ recipe.image }
-                alt={ recipe.name }
-                data-testid={ `${index}-horizontal-image` }
-              />
+      {!filteredItems.length
+        ? (
+          <div className="zero-done-recipes-container">
+            <h2>You don&apos;t have any completed recipe here.</h2>
+
+            <Link to={ `${selectedFilterIsCocktails ? '/cocktails' : '/meals'}` }>
+              Find one to cook now!
             </Link>
-
-            <Link
-              to={ `/${recipe.type}/${recipe.id}` }
-              data-testid={ `${index}-horizontal-name` }
-            >
-              {recipe.name}
-            </Link>
-
-            <p data-testid={ `${index}-horizontal-top-text` }>
-              {recipe.type === 'meals'
-                ? `${recipe.area} - ${recipe.category}`
-                : `${recipe.alcoholicOrNot}`}
-            </p>
-
-            <p
-              data-testid={ `${index}-horizontal-done-date` }
-            >
-              Cooked on:
-              {' '}
-              {recipe.doneDate}
-            </p>
-
-            <div className="done-recipe-share-container">
-              <span>Share this recipe:</span>
-
-              <button
-                type="button"
-                onClick={ () => shareWhenMultipleRecipesPresent(
-                  recipe.id, recipe.type, setCopyLink,
-                ) }
-              >
-                <img
-                  data-testid={ `${index}-horizontal-share-btn` }
-                  src={ shareIcon }
-                  alt="share this recipe"
-                />
-
-                {copyLink[recipe.id] && (
-                  <p>Copied Link!</p>
-                )}
-              </button>
-            </div>
-
-            {recipe.type === 'meals' && (
-              <div className="recipe-tag-container">
-                {recipe.tags.filter((tag, i) => i < 1 + 1).map((tag) => (
-                  <span
-                    key={ tag }
-                    data-testid={ `${index}-${tag}-horizontal-tag` }
-                  >
-                    {tag}
-                  </span>
-                ))}
-              </div>
-            )}
-
           </div>
-        ))}
-      </div>
+        ) : (
+          <div className="done-recipes-container">
+            {filteredItems.map((recipe, index) => (
+              <div className="done-recipe-card" key={ recipe.doneDate.toLocaleString() }>
+                <Link to={ `/${recipe.type}/${recipe.id}` }>
+                  <img
+                    src={ recipe.image }
+                    alt={ recipe.name }
+                    data-testid={ `${index}-horizontal-image` }
+                  />
+                </Link>
+
+                <Link
+                  to={ `/${recipe.type}/${recipe.id}` }
+                  data-testid={ `${index}-horizontal-name` }
+                >
+                  {recipe.name}
+                </Link>
+
+                <p data-testid={ `${index}-horizontal-top-text` }>
+                  {recipe.type === 'meals'
+                    ? `${recipe.area} - ${recipe.category}`
+                    : `${recipe.alcoholicOrNot}`}
+                </p>
+
+                <p
+                  data-testid={ `${index}-horizontal-done-date` }
+                >
+                  Cooked on:
+                  {' '}
+                  {recipe.doneDate}
+                </p>
+
+                <div className="done-recipe-share-container">
+                  <span>Share this recipe:</span>
+
+                  <button
+                    type="button"
+                    onClick={ () => shareWhenMultipleRecipesPresent(
+                      recipe.id, recipe.type, setCopyLink,
+                    ) }
+                  >
+                    <img
+                      data-testid={ `${index}-horizontal-share-btn` }
+                      src={ shareIcon }
+                      alt="share this recipe"
+                    />
+
+                    {copyLink[recipe.id] && (
+                      <p>Copied Link!</p>
+                    )}
+                  </button>
+                </div>
+
+                {recipe.type === 'meals' && (
+                  <div className="recipe-tag-container">
+                    {recipe.tags.filter((tag, i) => i < 1 + 1).map((tag) => (
+                      <span
+                        key={ tag }
+                        data-testid={ `${index}-${tag}-horizontal-tag` }
+                      >
+                        {tag}
+                      </span>
+                    ))}
+                  </div>
+                )}
+
+              </div>
+            ))}
+          </div>
+        )}
+
     </div>
   );
 }
--- src/pages/Favorites/index.jsx
+++ src/pages/Favorites/index.jsx
@@ -22,9 +22,9 @@ function Favorites() {
 
   const filteredItems = useMemo(() => {
     switch (filter) {
-    case 'Foods':
+    case 'meals':
       return favoriteRecipes.filter((recipe) => recipe.type === 'meals');
-    case 'Drinks':
+    case 'cocktails':
       return favoriteRecipes.filter((recipe) => recipe.type === 'cocktails');
     default:
       return favoriteRecipes;
@@ -43,6 +43,8 @@ function Favorites() {
     updateFavoriteRecipes(dataToUnfavorite, true);
   }, [updateFavoriteRecipes]);
 
+  const selectedFilterIsCocktails = useMemo(() => filter === 'cocktails', [filter]);
+
   return (
     <div className="favorite-recipes-page">
       <Header pageTitle="Favorite Recipes" />
@@ -52,67 +54,79 @@ function Favorites() {
         currentFilter={ filter }
       />
 
-      <div className="favorite-recipes-container">
-        {filteredItems.map((recipe, index) => (
-          <div className="favorite-recipe-card" key={ `${recipe.type}-${recipe.name}` }>
-            <Link to={ `/${recipe.type}/${recipe.id}` }>
-              <img
-                src={ recipe.image }
-                alt={ recipe.name }
-                data-testid={ `${index}-horizontal-image` }
-              />
+      {!filteredItems.length
+        ? (
+          <div className="zero-done-recipes-container">
+            <h2>You don&apos;t have any completed recipe here.</h2>
+
+            <Link to={ `${selectedFilterIsCocktails ? '/cocktails' : '/meals'}` }>
+              Find one to cook now!
             </Link>
-
-            <Link
-              to={ `/${recipe.type}/${recipe.id}` }
-              data-testid={ `${index}-horizontal-name` }
-            >
-              {recipe.name}
-            </Link>
-
-            <p data-testid={ `${index}-horizontal-top-text` }>
-              {recipe.type === 'meals'
-                ? `${recipe.area} - ${recipe.category}`
-                : `${recipe.alcoholicOrNot}`}
-            </p>
-
-            <div className="favorite-btn-container">
-
-              <div className="done-recipe-share-container">
-                <button
-                  type="button"
-                  onClick={ () => shareWhenMultipleRecipesPresent(
-                    recipe.id, recipe.type, setCopyLink,
-                  ) }
-                >
-                  <img
-                    src={ shareIcon }
-                    alt="share this recipe"
-                    data-testid={ `${index}-horizontal-share-btn` }
-                  />
-
-                  {copyLink[recipe.id] && (
-                    <p>Copied Link!</p>
-                  )}
-                </button>
-              </div>
-
-              <button
-                type="button"
-                onClick={ () => handleRecipeUnfavorite(recipe.id, recipe.type) }
-              >
-                <img
-                  data-testid={ `${index}-horizontal-favorite-btn` }
-                  src={ heartIcon }
-                  alt="unfavorite this recipe"
-                />
-              </button>
-
-            </div>
-
           </div>
-        ))}
-      </div>
+        ) : (
+          <div className="favorite-recipes-container">
+            {filteredItems.map((recipe, index) => (
+              <div className="favorite-recipe-card" key={ `${recipe.type}-${recipe.name}` }>
+                <Link to={ `/${recipe.type}/${recipe.id}` }>
+                  <img
+                    src={ recipe.image }
+                    alt={ recipe.name }
+                    data-testid={ `${index}-horizontal-image` }
+                  />
+                </Link>
+
+                <Link
+                  to={ `/${recipe.type}/${recipe.id}` }
+                  data-testid={ `${index}-horizontal-name` }
+                >
+                  {recipe.name}
+                </Link>
+
+                <p data-testid={ `${index}-horizontal-top-text` }>
+                  {recipe.type === 'meals'
+                    ? `${recipe.area} - ${recipe.category}`
+                    : `${recipe.alcoholicOrNot}`}
+                </p>
+
+                <div className="favorite-btn-container">
+
+                  <div className="done-recipe-share-container">
+                    <button
+                      type="button"
+                      onClick={ () => shareWhenMultipleRecipesPresent(
+                        recipe.id, recipe.type, setCopyLink,
+                      ) }
+                    >
+                      <img
+                        src={ shareIcon }
+                        alt="share this recipe"
+                        data-testid={ `${index}-horizontal-share-btn` }
+                      />
+
+                      {copyLink[recipe.id] && (
+                        <p>Copied Link!</p>
+                      )}
+                    </button>
+                  </div>
+
+                  <button
+                    type="button"
+                    onClick={ () => handleRecipeUnfavorite(recipe.id, recipe.type) }
+                  >
+                    <img
+                      data-testid={ `${index}-horizontal-favorite-btn` }
+                      src={ heartIcon }
+                      alt="unfavorite this recipe"
+                    />
+                  </button>
+
+                </div>
+
+              </div>
+            ))}
+          </div>
+        )}
+
     </div>
   );
 }
